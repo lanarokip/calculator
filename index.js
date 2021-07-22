5dafaefaf58db7bc74153f75eb6ebb6c297a648b
lanarokip <lankipkoecharo@gmail.com>
2021-07-22 12:42:13 +0300

    added the subtraction-division section

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function addition (a,b) {
-    return a+b;
+var add = function(num1,num2){
+    return num1+num2;
+};
+var subtract = function(num1,num2){
+    return num1-num2;
+};
+var multiply = function(num1,num2){
+    return num1*num2;
+};
+var devide = function(num1,num2){
+    return num1/num2;
 }
-var a = parseInt(prompt("Enter a number:"));
-var b = parseInt(prompt("enter the other number"));
-alert(addition(a,b));
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("enter the other number"));
+alert(add(num1,num2));
