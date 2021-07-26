cabbc6c44763a8db1858bd0c0a17b4b6d0874057
lanarokip <lankipkoecharo@gmail.com>
2021-07-26 07:53:40 +0300

    initialized the html section

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ var multiply = function(num1,num2){
 };
 var devide = function(num1,num2){
     return num1/num2;
-}
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("enter the other number"));
-alert(add(num1,num2));
+};
+
+$(document).ready(function() {
+    $("form#add").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    $("#output").text(result);
+    });
+    });
